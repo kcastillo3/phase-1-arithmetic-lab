eac58b24a8c5278c03a8ca7041c211ddf0ecf815
Kevin <kevincastillo333@gmail.com>
2023-12-17 23:11:11 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-// Write your code here
+// // Write your code here
+// let num1 = 31;
+// let num2 = 2;
+// let multiply = num1 * num2;
+
+// let random = Math.floor(Math.random() * 10) + 1;
+
+// let num3 = 39; 
+// let num4 = 5;
+// let mod = num3 % num4;
+
+// let max = Math.max(12,14,16,18,20);
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 39;
+let num4 = 5;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; // Generates a random integer greater than 0
+let mod = num3 % num4;
+let max = Math.max(10, 15, 20, 18);
+
+console.log("Multiply:", multiply); // Output: Multiply: 62
+console.log("Random:", random);     // Output: Random: (some random integer)
+console.log("Mod:", mod);           // Output: Mod: 3
+console.log("Max:", max);           // Output: Max: 20
